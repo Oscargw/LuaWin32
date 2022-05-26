b4d82fcfe429f11a7696f273de880a7e3f0a208c
Oscargw <274607900@qq.com>
2022-05-26 16:29:10 +0800

    luawin32编译通过,lua5.4.4

---
+++ luaWin32/luaWin32.cpp
@@ -0,0 +1,33 @@
+﻿// luaWin32.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+
+extern "C"
+{
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    luaL_dostring(L, "print('hello lua !!')");
+    // luaL_dofile(L, "main.lua");
+
+    lua_close(L);
+
+
+    std::cout << "Bye Bye !!\n";
+
+    system("pause");
+
+    return 0;
+}
